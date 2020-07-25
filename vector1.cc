7f619fad6aaa156fa76f91d94e3487869093e352
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-07-25 23:50:27 +0300

    more simplification

--- vector1.cc
+++ vector1.cc
@@ -17,16 +17,14 @@ int main(){
    v.push_back( 3 );  
    v.push_back( 4 );
 
-   cout << v.size() << "\n" << v.capacity() << "\n";
+   cout << v.size() << endl << v.capacity() << endl;
    printVector( v );
 
-   vector< int >::const_reverse_iterator reverseIterator; 
-   vector< int >::const_reverse_iterator tempIterator = v.rend();
+   vector< int >::const_reverse_iterator revIter; 
    cout << endl;
 
-   for ( reverseIterator = v.rbegin();               
-      reverseIterator!= tempIterator; ++reverseIterator )
-      cout << *reverseIterator << ' ';            
+   for ( revIter = v.rbegin(); revIter!= v.rend(); ++revIter )
+      cout << *revIter << ' ';            
 
    cout << endl;
 } 
