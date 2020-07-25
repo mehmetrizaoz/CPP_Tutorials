96c091ece137ede3bdb710654f61aa807eb24369
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-07-25 23:44:42 +0300

    code was refactored

--- vector1.cc
+++ vector1.cc
@@ -1,51 +1,32 @@
-//Standard Library vector class template.
 #include <iostream>
-#include <vector> // vector class-template definition
+#include <vector> 
+
 using namespace std;
 
-void printVector( const vector< int > &v )
-{
-   vector< int >::const_iterator constIterator; // const_iterator
+void printVector( const vector< int > &v ){
+   vector< int >::const_iterator constIterator; 
 
-   // display vector elements using const_iterator
    for ( constIterator = v.begin(); 
       constIterator != v.end(); ++constIterator )
       cout << *constIterator << ' ';
 } 
 
 int main(){
-   const int SIZE = 6; // define array size
-   int array[ SIZE ] = { 1, 2, 3, 4, 5, 6 }; // initialize array
-   vector< int > integers; // create vector of ints
+   vector< int > v;
+   v.push_back( 2 );  
+   v.push_back( 3 );  
+   v.push_back( 4 );
 
-   cout << "The initial size of integers is: " << integers.size()
-      << "\nThe initial capacity of integers is: " << integers.capacity();
+   cout << v.size() << "\n" << v.capacity() << "\n";
+   printVector( v );
 
-   // function push_back is in every sequence container
-   integers.push_back( 2 );  
-   integers.push_back( 3 );  
-   integers.push_back( 4 );
-
-   cout << "\nThe size of integers is: " << integers.size()
-      << "\nThe capacity of integers is: " << integers.capacity();
-   cout << "\n\nOutput array using pointer notation: ";
-
-   // display array using pointer notation
-   for ( int *ptr = array; ptr != array + SIZE; ++ptr )
-      cout << *ptr << ' ';
-
-   cout << "\nOutput vector using iterator notation: ";
-   printVector( integers );
-   cout << "\nReversed contents of vector integers: ";
-
-   // two const reverse iterators
    vector< int >::const_reverse_iterator reverseIterator; 
-   vector< int >::const_reverse_iterator tempIterator = integers.rend();
+   vector< int >::const_reverse_iterator tempIterator = v.rend();
+   cout << endl;
 
-   // display vector in reverse order using reverse_iterator
-   for ( reverseIterator = integers.rbegin();               
+   for ( reverseIterator = v.rbegin();               
       reverseIterator!= tempIterator; ++reverseIterator )
-      cout << *reverseIterator << ' ';       
+      cout << *reverseIterator << ' ';            
 
    cout << endl;
 } 
