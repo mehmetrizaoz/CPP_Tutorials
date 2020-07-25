458ff85093740ce90d138b706d89c491d3d9d357
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-07-25 23:26:30 +0300

    comments were deleted

--- vector1.cc
+++ vector1.cc
@@ -1,5 +1,4 @@
-// Fig. 22.14: Fig22_14.cpp
-// Demonstrating Standard Library vector class template.
+//Standard Library vector class template.
 #include <iostream>
 #include <vector> // vector class-template definition
 using namespace std;
@@ -56,17 +55,3 @@ template < typename T > void printVector( const vector< T > &integers2 )
       cout << *constIterator << ' ';
 } // end function printVector
 
-/**************************************************************************
- * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- **************************************************************************/
