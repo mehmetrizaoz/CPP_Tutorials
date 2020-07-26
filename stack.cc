62b6242cf5e25ff321a4ee96fe1fba131095cfbb
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-07-27 00:08:59 +0300

    stack file was added

---
+++ stack.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+
+template< typename T > void pushElements( T &stackRef )
+{
+   for ( int i = 0; i < 10; ++i )
+   {
+      stackRef.push( i );
+      cout << stackRef.top() << ' '; 
+   } 
+}
+
+template< typename T > void popElements( T &stackRef )
+{
+   while ( !stackRef.empty() ) 
+   {
+      cout << stackRef.top() << ' ';
+      stackRef.pop(); 
+   }
+
+} 
+int main()
+{
+   stack< int > intDequeStack;   
+   stack< int, vector< int > > intVectorStack;
+   stack< int, list< int > > intListStack;
+
+   cout << "Pushing onto intDequeStack: ";
+   pushElements( intDequeStack );
+   cout << "\nPushing onto intVectorStack: ";
+   pushElements( intVectorStack );
+   cout << "\nPushing onto intListStack: ";
+   pushElements( intListStack );
+   cout << endl << endl;
+
+   cout << "Popping from intDequeStack: ";
+   popElements( intDequeStack );
+   cout << "\nPopping from intVectorStack: ";
+   popElements( intVectorStack );
+   cout << "\nPopping from intListStack: ";
+   popElements( intListStack );
+   cout << endl;
+}
+
