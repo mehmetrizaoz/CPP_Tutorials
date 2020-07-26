d6ccb53da69d49563f33aecae75b7556fe50097c
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-07-26 15:16:13 +0300

    more simplification

--- list1.cc
+++ list1.cc
@@ -21,48 +21,34 @@ int main(){
    v.push_front( 1 );
    v.push_front( 2 );
    v.push_back( 4 );
-   v.push_back( 3 );  
-    
-   printList( v );
-
-   v.sort();
-   printList( v );
-  
+   v.push_back( 3 );    
+   
    v2.insert( v2.begin(), array, array + SIZE );
-   cout << "insert: ";      
-   printList( v2 );
-
-   v.splice( v.end(), v2 );
-   cout << "splice: ";   
-   printList( v );
-
-   v2.insert( v2.begin(), array, array + SIZE );
+   
    v2.sort();
-   cout << "sort: ";
-   printList( v2 );
+   v.sort();   
    
    v.merge( v2 );                         
-   cout << "merge: ";
    printList( v );
 
+   v2.insert( v2.begin(), array, array + SIZE );
+
+   v.splice( v.end(), v2 );
+   printList( v );
+   
    v.pop_front();
    v.pop_back();
    printList( v );
 
    v.unique();
-   cout << "unique: ";
    printList( v );
    
    v.swap( v2 );
-   printList( v );
+   printList( v2 );
 
    v.assign( v2.begin(), v2.end() );
    printList( v );
 
-   v.merge( v2 ); 
-   printList( v );
-   
    v.remove( 4 );
    printList( v );
-   cout << endl;
 } 
