e87fcc74e27b6f3b0ee7cda241a72aa9d93f7031
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-08-03 14:30:47 +0300

    inheritance example creation

---
+++ inheritance/test.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include "Child.h"
+
+int main(){
+   Child myChild;
+   Child mySecondChild("mehmet", "oz");
+   string word;
+   
+   cout << "my name is " << myChild.getName() << " ";
+   cout << myChild.getSirName() << endl;
+   
+   myChild.setName("ali").setSirName("kaya");   
+   
+   getline( cin, word );  
+   myChild.speak("You told me to say " + word); 
+   
+   cout << "object count " << Child::getCount() << endl; //static func. call
+}
+
+
