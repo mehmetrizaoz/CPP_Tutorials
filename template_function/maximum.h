6a6bba77a709ac658db39c29a817468b87a4db45
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-08-07 12:43:25 +0300

    another template function was added

--- template_function/maximum.h
+++ template_function/maximum.h
@@ -1,4 +1,8 @@
-template < class T > 
+#include <iostream>
+
+using namespace std;
+
+template < typename T >
 T maximum( T value1, T value2, T value3 )
 {
    T maximumValue = value1; 
@@ -10,5 +14,12 @@ T maximum( T value1, T value2, T value3 )
       maximumValue = value3;
 
    return maximumValue;
+}
+
+template < typename T >
+void printArray ( const T * const array, int count ){
+   for( int i=0; i<count; ++i)
+      cout << array[i] << " ";
+   cout << endl;
 }
 
--- template_function/test.cc
+++ template_function/test.cc
@@ -27,5 +27,17 @@ int main(){
 
    cout << "The maximum character value is: "
       << maximum( char1, char2, char3 ) << endl;
+      
+   const int aCount = 5; 
+   const int bCount = 7; 
+   const int cCount = 6; 
+
+   int a[ aCount ] = { 1, 2, 3, 4, 5 };
+   double b[ bCount ] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+   char c[ cCount ] = "HELLO"; 
+
+   printArray( a, aCount );
+   printArray( b, bCount );   
+   printArray( c, cCount );   
 } 
 
