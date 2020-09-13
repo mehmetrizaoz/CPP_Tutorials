a7fa1c31838180a5c6a534f2ee84e918dd0bb326
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-09-14 01:04:50 +0300

    lambda code

---
+++ C++11/lambda_functions/test.cc
@@ -0,0 +1,93 @@
+/*
+[ capture list ] (parameters) -> return-type  
+{   
+    method definition
+} 
+
+[ ](){} 	no captures
+[=](){} 	captures everything by copy (not recommendded)
+[&](){} 	captures everything by reference (not recommendded)
+[x](){} 	captures x by copy
+[&x](){} 	captures x by reference
+[&, x](){} 	captures x by copy, everything else by reference
+[=, &x](){} 	captures x by reference, everything else by copy
+
+*/
+#include <bits/stdc++.h> 
+
+using namespace std; 
+
+class OddCounter
+{
+   int mCounter = 0;
+public:
+   int getCount(){
+       return mCounter;
+   }
+   void update(vector<int> & vec){
+      for_each(vec.begin(), vec.end(), [this](int element){
+         if(element % 2 == 0)
+            mCounter++;
+      });
+   }     
+};
+//-------------------------
+void foo() { std::cout << "foo()\n"; }
+void bar() { std::cout << "bar()\n"; }
+//-------------------------
+
+int main() { 
+   vector<int> v {4, 1, 3, 5, 2, 3, 1, 7}; 
+   for_each(v.begin(), v.end(), [](int element){cout << element << " ";});   
+   cout << endl;
+//-------------------------   
+   int sum = 0;
+   for_each(v.begin(), v.end(), [ &sum ]( int i ){ sum += i; } );
+   cout << "sum: " << sum << endl;
+//-------------------------
+   for_each(v.begin(), v.end(), [](int& v) {static int n = 1; v = n++;});
+   for_each(v.begin(), v.end(), [](int i) {cout << i << " ";});
+   cout << endl;
+//-------------------------
+   int x = 100, y = 200;
+   auto p = [&](){cout << __PRETTY_FUNCTION__ <<" : "<<sum<<","<<y<<endl;};
+   p();   
+//-------------------------
+   auto f  = []() {
+      foo();
+      bar();
+   };
+   f();
+//-------------------------   
+   auto g = [](int a, int b)->int{ return a*b; };
+   cout << g(4, 5) << endl;   
+//-------------------------      
+   cout << [](int a, int b)->int {return a*b;}(4, 5) << endl;  
+//-------------------------      
+   cout << [](int n)->int {return n*n;}(5) << endl;      
+//-------------------------        
+   auto k = [&sum](int a, int b){ sum = a*b; };
+   k(5,7);
+   cout << sum << endl;  
+//-------------------------
+   vector<int> vec = {12,3,2,1,8,9,0,2,3,9,7};
+   OddCounter counterObj;
+   counterObj.update(vec);
+   int count = counterObj.getCount();
+   cout<<"Counter = "<<count<<endl;
+//-------------------------       
+   cout << count_if(vec.begin(), vec.end(), [](int x)->int{return x>5;})<<endl;
+//-------------------------
+//generic lambda
+   auto ff = [](auto a, auto b){return a*b;};
+   cout<<ff(7,6)<<endl;
+   cout<<ff(1.1, 2.0)<<endl;
+ 
+} 
+
+
+
+
+
+
+
