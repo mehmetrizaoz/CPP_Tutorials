ae2890a639e6df82da8bad2efc16ca0d9959050e
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-09-13 15:40:58 +0300

    variadic template function

---
+++ C++11/variadic_template/test.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+template < typename T >
+T add(const T arg){
+  return arg;
+}
+
+template < typename T, typename... ARGS >
+T add(const T arg, const ARGS... args){
+  return arg + add(args...);
+}
+
+
+void log(){
+}
+
+template<typename T, typename ... Args>
+void log(T first, Args ... args) {
+    std::cout<<first<<" ";
+    log(args ...);
+}
+
+
+int main(){
+  cout << add(1, 2, 3, 4, 5, 6) << endl;
+  cout << add(1.1, 2.4, 3.4, 5.6) << endl;  
+  
+  log(2, 3.4, "aaa"); cout << endl;
+  return 0;
+}
