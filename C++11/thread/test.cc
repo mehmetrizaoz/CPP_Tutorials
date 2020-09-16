fc80b77be0002ccfa47e80b24ddaf45278a021ca
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-09-16 19:57:17 +0300

    thread code

---
+++ C++11/thread/test.cc
@@ -0,0 +1,46 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+using namespace std; 
+
+void foo(){
+  cout << "foo enter" << endl;
+  for (int i=0; i<5; i++){
+     cout << endl <<"hhhhhhhhh" << endl;
+     this_thread::sleep_for(chrono::seconds(1));
+  }
+  cout << "foo exit" << endl;
+}
+
+void bar(int x){
+  cout << "bar enter" << endl;
+  this_thread::sleep_for(chrono::milliseconds(7000));
+  cout << "bar exit" << endl;
+}
+
+int main(){
+  thread first (foo);     
+  thread second (bar, 0); 
+  
+  cout<< this_thread::get_id() << endl;
+  cout << "main, foo and bar now execute concurrently...\n";
+  
+  first.detach();  
+  
+  if (first.joinable()){
+     cout << "first join" << endl;
+     first.join();
+  }
+  if (second.joinable()){
+     cout << "second join" << endl;  
+     second.join();  
+  }
+  
+  cout << "foo and bar completed.\n";
+  return 0;
+}
+
+
+
+
