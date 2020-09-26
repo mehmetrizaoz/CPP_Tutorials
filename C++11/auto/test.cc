10306e1056da10e00ca9c0a8929dd6009bed1cba
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-09-26 23:26:31 +0300

    decltype

--- C++11/auto/test.cc
+++ C++11/auto/test.cc
@@ -1,9 +1,31 @@
 #include<iostream>
 #include <vector> 
-
+#include <map>
+  
 using namespace std;
+
+auto meaningOfLife()
+{
+   return 42;
+}
+
+template<typename T, typename U>
+auto add (T t, U u)
+{
+   return t+u;
+}
  
 int main(){
+    cout << meaningOfLife() << endl;
+    cout << add(3, 4.5) << endl;
+    cout << add(string("ali"), string(" oz")) << endl;    
+
+    vector<double> values;
+    typedef decltype(values.begin()) dvi; //double vector iterator    
+    typedef decltype(42) myint;
+    typedef decltype(add(3, 4.5)) qtype;
+    
+
     vector< int > v = { 1, 2, 3, 4, 5, 6 };
     
     //vector< int >::const_iterator iter; 
@@ -18,5 +40,11 @@ int main(){
 
     cout << no << " " << name << " " << wage << endl;
 
+    auto d = {1,2,3}; //list
+    map<string, vector<double>> ticks;
+    //map<string, vector<double>>::iterator it = ticks.begin();
+    auto it = ticks.begin();
+    
+    
     return 0;
 }
