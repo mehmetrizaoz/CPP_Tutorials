be0bf2a10c8a41c704fe639379c6bfdfeae8c077
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-09-27 12:14:51 +0300

    range based for loop

---
+++ C++11/range_based_for_loop/test.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <vector> 
+#include <map>
+  
+using namespace std;
+
+int main() { 
+   // Iterating over whole array 
+   std::vector<int> v = {0, 1, 2, 3, 4, 5}; 
+   for (auto i : v) 
+      cout << i << ' ';    
+   cout << '\n'; 
+   
+   // the initializer may be a braced-init-list 
+   for (int n : {0, 1, 2, 3, 4, 5}) 
+      cout << n << ' ';    
+   cout << '\n'; 
+
+   // Iterating over array 
+   int a[] = {0, 1, 2, 3, 4, 5};    
+   for (int n : a) 
+      cout << n << ' ';    
+   cout << '\n'; 
+   
+   // Just running a loop for every array 
+   // element 
+   for (int n : a) 
+      cout << "In loop" << ' ';    
+   cout << '\n'; 
+   
+   // Printing string characters 
+   string str = "Geeks"; 
+   for (char c : str) 
+      cout << c << ' ';       
+   cout << '\n'; 
+
+   // Printing keys and values of a map 
+   map <int, int> MAP({{1, 1}, {2, 2}, {3, 3}}); 
+   for (auto i : MAP) 
+      cout << '{' << i.first << ", " << i.second << "}\n"; 
+} 
+
