4c2a429e9c0c452222dafdb59a129d1dca813cd9
mehmetrizaoz <mehmetrizaoz@gmail.com>
2020-10-26 20:24:44 +0300

    virtual destructor

---
+++ virtual_destructor/test.cc
@@ -0,0 +1,30 @@
+// https://www.geeksforgeeks.org/virtual-destructor/
+#include<iostream> 
+
+using namespace std; 
+
+class base { 
+public: 
+	base()	 
+	{ cout<<"Constructing base \n"; } 
+	virtual ~base() 
+	{ cout<<"Destructing base \n"; }	 
+}; 
+
+class derived: public base { 
+public: 
+	derived()	 
+	{ cout<<"Constructing derived \n"; } 
+	~derived() 
+	{ cout<<"Destructing derived \n"; } 
+}; 
+
+int main(void) 
+{ 
+derived *d = new derived(); 
+base *b = d; 
+delete b; 
+getchar(); 
+return 0; 
+} 
+
